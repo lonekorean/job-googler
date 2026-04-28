a650304757924b39e9569a8b0d8e7870cff02de9
Will Boyd <will@codersblock.com>
2026-04-28 10:14:38 -0400

    basic routing, stubbed about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import './App.css';
+import { Link, Route, Routes } from 'react-router';
+import About from './pages/About';
+import Home from './pages/Home';
+import './styles/App.css';
 
 export default function App() {
   return (
-    <main>
-      <h1>Job Googler!</h1>
-    </main>
+    <>
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/About">About</Link>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </main>
+    </>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import '../styles/pages/Home.css';
+
+export default function Home() {
+  return (
+    <h1>Home</h1>
+  );
+}
